99821c459d62ca6c996ed3ff188916fd416920cb
Barrett <barrettpyke@gmail.com>
2021-10-13 18:23:54 -0500

    updates

--- script.js
+++ script.js
@@ -1,44 +1,77 @@
+const display = document.getElementById('display-text');
+const numButtons = document.querySelectorAll('.num');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('#equals');
+let operandA = null;
+let operandB = null;
+let operator = null;
+let result = 0;
+
 function add(x, y) {
-    x + y;
+    result = x + y;
 }
 
 function subtract(x, y) {
-    x - y;
+    result = x - y;
 }
 
 function multiply(x, y) {
-    x * y;
+    result = x * y;
 }
 
 function divide(x, y) {
-    x / y;
+    result = x / y;
 }
 
 function operate(operator, x, y) {
-    if (operator === '+') {
+    if (operator === 'add') {
         add(x, y);
-    } else if (operator === '-') {
+    } else if (operator === 'subtract') {
         subtract(x, y);
-    } else if (operator === '*') {
+    } else if (operator === 'multiply') {
         multiply(x, y);
-    } else if (operator === '/') {
+    } else if (operator === 'divide') {
         divide(x, y)
+    }
+    return result
+}
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        addText(button.innerText);
+    })
+})
+
+function addText(value) {
+    if (display.textContent.length < 10) {
+    display.textContent += value;
     }
 }
 
-const textContent = document.getElementById('text-content');
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        operandA = parseFloat(display.textContent);
+        display.textContent = null;
+        if (button.id === 'add') {
+            operator = 'add';
+        } else if (button.id === 'subtract') {
+            operator = 'subtract';
+        } else if (button.id === 'multiply') {
+            operator = 'multiply';
+        } else if (button.id === 'divide') {
+            operator = 'divide';
+        }
+    })
+})
 
-const one = document.getElementById('1');
-const two = document.getElementById('2');
-const three = document.getElementById('3');
-const four = document.getElementById('4');
-const five = document.getElementById('5');
-const six = document.getElementById('6');
-const seven = document.getElementById('7');
-const eight = document.getElementById('8');
-const nine = document.getElementById('9');
-const zero = document.getElementById('0');
+equalsButton.addEventListener('click', () => {
+    operandB = parseFloat(display.textContent);
+    operate(operator, operandA, operandB);
+    display.textContent = result;
+    operandA = result;
+    operandB = null;
+})
 
-function addText() {
-    
-}
+//add listener for decimal let it be disabled after click and reset after add or equals
+//figure out how to disable equals
+//add clear button
